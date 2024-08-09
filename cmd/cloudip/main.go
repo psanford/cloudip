b9157e6986a582e6862c602824d46c5dbdece4cf
Peter Sanford <psanford@sanford.io>
2024-08-09 11:43:01 -0700

    Make cli output nicer

--- cmd/cloudip/main.go
+++ cmd/cloudip/main.go
@@ -1,35 +1,78 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/netip"
 	"os"
+	"strings"
 
 	"github.com/psanford/cloudip"
 )
 
+var format = flag.String("format", "text", "Output format (text|csv|json)")
+
 func main() {
-	if len(os.Args) < 2 {
+	flag.Parse()
+	if *format != "text" && *format != "csv" && *format != "json" {
+		log.Fatalf("invalid -format. Valid values are: text,csv,json")
+	}
+
+	if len(flag.Args()) < 1 {
 		log.Fatalf("Usage: %s <ip>\n", os.Args[0])
 	}
 
-	ipStr := os.Args[1]
+	csvOut := csv.NewWriter(os.Stdout)
+	jsonOut := json.NewEncoder(os.Stdout)
+	jsonOut.SetIndent("", "  ")
 
-	addr, err := netip.ParseAddr(ipStr)
-	if err != nil {
-		log.Fatalf("ip parse error: %s", err)
+	if *format == "csv" {
+		csvOut.Write([]string{"prefix", "provider", "region", "services"})
 	}
 
-	r := cloudip.Lookup(addr)
-	if r == nil {
-		os.Exit(1)
+	exitCode := 0
+
+	for _, ipStr := range flag.Args() {
+
+		addr, err := netip.ParseAddr(ipStr)
+		if err != nil {
+			log.Fatalf("ip parse error: %s", err)
+		}
+
+		r := cloudip.Lookup(addr)
+		if r == nil {
+			log.Printf("ip %s not found", addr)
+			exitCode = 1
+			continue
+		}
+
+		switch *format {
+		case "text":
+			fmt.Printf("Prefix:   %s\n", r.Prefix)
+			fmt.Printf("Provider: %s\n", r.Provider)
+			if r.Region != "" {
+				fmt.Printf("Region:   %s\n", r.Region)
+			}
+			if len(r.Services) > 0 {
+				fmt.Printf("Service:  %s\n", strings.Join(r.Services, ","))
+			}
+			if len(flag.Args()) > 1 {
+				fmt.Println("==================================================")
+			}
+		case "csv":
+			services := strings.Join(r.Services, ";")
+			csvOut.Write([]string{r.Prefix.String(), string(r.Provider), r.Region, services})
+			csvOut.Flush()
+		case "json":
+			jsonOut.Encode(r)
+		}
 	}
 
-	b, err := json.MarshalIndent(r, "", "  ")
-	if err != nil {
-		panic(err)
+	if exitCode != 0 {
+		log.Print("Error: Some Lookups failed")
 	}
-	fmt.Printf("%s\n", b)
+	os.Exit(exitCode)
 }
